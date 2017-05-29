197174d4886b3d2600efe00cbb57164fb7f4c906
fscfelipe <felipesobreira.c@hotmail.com>
2017-05-29 20:16:08 -0300

    algumas documentações para guiar

--- zeroProject/src/pages/home/home.ts
+++ zeroProject/src/pages/home/home.ts
@@ -6,9 +6,26 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  //AQUI PODEMOS DECLARAR VARIÁVEIS QUE PODERÃO SER MANIPULADAS
+  // PARA SEREM ADICIONADAS FUNCIONALIDADES, COMO A LISTA DE OBJETOS
+  // QUE SERÃO "IMPORTADOS" DE UM BANCO EM UM SERVIDOR
+  // EX: public x: number = 0;
+  // Para acessar a variável no página html, usar {{x}}
 
   constructor(public navCtrl: NavController) {
 
   }
+
+  //Aqui em baixo, poderão ser adicionador método que irão manipular
+  //variáveis, ou irão retornar dados para a página html.
+  //ex: 
+  // printValor(){
+  //    return this.x; 
+  // }
+  //Esse método pode ser acessado pela página html da seguinte forma
+  //ex: <ion-buttons>
+  //      <button ion-button block (click)="printValor()">Printar</button>
+  //    </ion-buttons>
+  //
 
 }
