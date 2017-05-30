bc38c7904c43d1fab7c6ab6a4ad5a36f4b57e1dc
fscfelipe <felipesobreira.c@hotmail.com>
2017-05-30 00:49:12 -0300

    inicio mapa sem bug

--- zeroProject/src/pages/map-page/map-page.ts
+++ zeroProject/src/pages/map-page/map-page.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
+
+//import adicionado para o inserir o mapa e funcionalidades
+// ver: https://ionicframework.com/docs/native/google-maps/
+import {
+ GoogleMaps,
+ GoogleMap,
+ GoogleMapsEvent,
+ LatLng,
+ CameraPosition,
+ MarkerOptions,
+ Marker
+} from '@ionic-native/google-maps';
 
 /**
  * Generated class for the MapPage page.
@@ -13,12 +25,46 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'map-page.html',
 })
 export class MapPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MapPage');
-  }
-
+  map: GoogleMap;
+ 
+    constructor(public navCtrl: NavController, public platform: Platform) {
+        platform.ready().then(() => {
+            this.loadMap();
+        });
+    }
+ 
+    loadMap(){
+ 
+        let location = new LatLng(-34.9290,138.6010);
+ 
+        this.map = new GoogleMap('map', {
+          'backgroundColor': 'white',
+          'controls': {
+            'compass': true,
+            'myLocationButton': true,
+            'indoorPicker': true,
+            'zoom': true
+          },
+          'gestures': {
+            'scroll': true,
+            'tilt': true,
+            'rotate': true,
+            'zoom': true
+          },
+          'camera': {
+            'latLng': location,
+            'tilt': 30,
+            'zoom': 15,
+            'bearing': 50
+          }
+        });
+ 
+        this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+            console.log('Map is ready!');
+        });
+ 
+    }
 }
+
+ 
+
